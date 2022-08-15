7d505cdbdb689cf752436222b36a451ce35a0896
Ezgi Akkas <ezgituncalp@hotmail.com>
2022-08-15 16:11:34 +0300

    second commit

---
+++ src/week02/lesson001/FirstClass.java
@@ -0,0 +1,10 @@
+package week02.lesson001;
+
+public class FirstClass {
+
+	public static void main(String[] args) {
+		// TODO Ezgi Akkaş
+
+	}
+
+}
--- src/week02/lesson001/Javaclass.java
+++
@@ -1,5 +0,0 @@
-package week02.lesson001;
-
-public class Javaclass {
-
-}
